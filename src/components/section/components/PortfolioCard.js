ebaf046ecfe07322dc9dcb4f5dfeb7dea61acc9b
Sorokin-Oleg <sorokin.world@gmail.com>
2019-04-11 17:59:01 +0300

    - Configure react-slick;

---
+++ src/components/section/components/PortfolioCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PortfolioCard extends React.Component {
+    render() {
+        return (
+            <div className='portfolio-card'>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default PortfolioCard
