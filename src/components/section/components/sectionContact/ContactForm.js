5e6b14a312b16198df19f7ce304484e7d94363e7
Sorokin-Oleg <sorokin.world@gmail.com>
2019-04-18 22:49:21 +0300

    - Creating a feedback form logic on the Contacts page;

--- src/components/section/components/sectionContact/ContactForm.js
+++ src/components/section/components/sectionContact/ContactForm.js
@@ -3,22 +3,56 @@ import React, {Component} from 'react';
 class ContactForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    render () {
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    render() {
         return (
             <div className='form-container'>
                 <div className='form-head'>        
                     <h1>Contact me</h1>
                     <p>This form is just an example. To contact me please use <a href="">LinkedIn</a></p>
-                </div>                
-                <form className='form-content'>
+                </div>
+                <form className='form-content' autoComplete="off" onSubmit={this.handleSubmit}>
                     <div className='form-content-header'>                       
-                        <input className='form-content-header-item' type="text" placeholder='Name' />                     
-                        <input className='form-content-header-item' type="email" placeholder='Email' />                       
+                        <input className='form-content-header-item' type="text" placeholder='Name'
+                               name='name' value={this.state.name} onChange={this.handleInputChange}
+                               maxLength='40' autoFocus required /> 
+
+                        <input className='form-content-header-item' type="email" placeholder='Email'
+                               name='email' value={this.state.email} onChange={this.handleInputChange}
+                               maxLength='40' required />
                     </div>                        
-                    <input type="text" placeholder='Subject' />               
-                    <textarea name="message" placeholder='Message' ></textarea>               
+                    <input type="text" placeholder='Subject' maxLength='60'
+                           name='subject' value={this.state.subject} onChange={this.handleInputChange} />
+                           
+                    <textarea placeholder='Message' maxLength='1000' required
+                              name='message' value={this.state.message} 
+                              onChange={this.handleInputChange}></textarea>  
+
                     <input type="submit" value='Send'/>                        
                 </form>
             </div>
