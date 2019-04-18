35a4cd5dbb7e0e74cead735b425726a410e4cd45
Sorokin-Oleg <sorokin.world@gmail.com>
2019-04-18 10:41:05 +0300

    - Removed the map on the Contacts page. An animated planet is added instead of a map.

---
+++ src/components/section/components/sectionContact/Earth.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Earth = () => {
+    return (
+        <div className='earth-container'>
+            <div className="earth"></div>
+        </div>
+    )
+}
+
+export default Earth;
