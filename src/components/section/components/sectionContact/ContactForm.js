768015960be7c7e0c8001ac37167425f83e32b6c
Sorokin-Oleg <sorokin.world@gmail.com>
2019-04-20 16:35:10 +0300

    - Implementation of the functional part of the Contacts page;

--- src/components/section/components/sectionContact/ContactForm.js
+++ src/components/section/components/sectionContact/ContactForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ModalContactForm from './modalContactForm/ModalContactForm';
 
 class ContactForm extends React.Component {
     constructor(props) {
@@ -8,11 +9,15 @@ class ContactForm extends React.Component {
             name: '',
             email: '',
             subject: '',
-            message: ''
-        }
+            message: '',
+            modal: false
+        };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.escCloseModal = this.escCloseModal.bind(this);
+        this.copyMessage = this.copyMessage.bind(this);        
     }
 
     handleInputChange(event) {
@@ -26,15 +31,50 @@ class ContactForm extends React.Component {
     }
 
     handleSubmit(event) {
+        this.setState({modal:true});
         event.preventDefault();
     }
 
-    render() {
+    /**
+     * Copy text to clipboard
+     */
+    copyMessage() {
+        navigator.clipboard.writeText(this.state.message);
+        this.closeModal();
+    }
+
+    /**
+     * When closing the modal window we clean the forms
+     */
+    closeModal() {
+        this.setState({modal:false});
+        this.setState({name:'',email:'',subject:'',message:''})
+    }
+
+    /**
+     * The function monitors the status of the modal window and the events of the pressed ESC button.
+     * Subject to the conditions, runs the function of closing the modal window
+     */
+    escCloseModal(event) {
+        if(event.keyCode === 27 && this.state.modal) {
+            this.closeModal();
+        }; 
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.escCloseModal, false);
+    }
+    
+    componentWillUnmount() {
+        document.addEventListener('keydown', this.escCloseModal, false);
+    }
+
+    render () {
         return (
             <div className='form-container'>
                 <div className='form-head'>        
                     <h1>Contact me</h1>
-                    <p>This form is just an example. To contact me please use <a href="">LinkedIn</a></p>
+                    <p>This form is just an example. To contact me please use <a href="https://www.linkedin.com/" target='_blank'>LinkedIn</a>.</p>
                 </div>
                 <form className='form-content' autoComplete="off" onSubmit={this.handleSubmit}>
                     <div className='form-content-header'>                       
@@ -55,6 +95,8 @@ class ContactForm extends React.Component {
 
                     <input type="submit" value='Send'/>                        
                 </form>
+                <ModalContactForm modal={this.state.modal} name={this.state.name}
+                                  closeModal={this.closeModal} copyMessage={this.copyMessage} />
             </div>
         )
     }
