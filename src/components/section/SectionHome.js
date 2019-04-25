cc6c55641a9a7b3f7ce15e86e12bf3e542ae26fb
Sorokin-Oleg <sorokin.world@gmail.com>
2019-04-25 20:16:07 +0300

    - Configure and implement a smooth transition between pages; - Configure react-router; - Fixed bug and refactoring;

--- src/components/section/SectionHome.js
+++ src/components/section/SectionHome.js
@@ -3,13 +3,15 @@ import Stars from './components/sectionHome/Stars';
 
 const SectionHome = () => {
     return (
-        <section className='content home-container'>
-            <Stars />
-            <div className='home-content'>                    
-                <img src="assets/img/home_logo.svg" alt="Sorokin Oleg - Frontend Developer"/>  
-                <h1><span>{'<h1>'}</span>{"Hi, I'm Sorokin Oleg"}<span>{'<h1>'}</span></h1>                  
-                <h1><span>{'<'}</span>{'Frontend Developer '}<span>{'/>'}</span></h1>
-            </div>
+        <section className='content'>
+            <div className='home-container'>
+                <Stars />
+                <div className='home-content'>                    
+                    <img src="assets/img/home_logo.svg" alt="Sorokin Oleg - Frontend Developer"/>  
+                    <h1><span>{'<h1>'}</span>{"Hi, I'm Sorokin Oleg"}<span>{'<h1>'}</span></h1>                  
+                    <h1><span>{'<'}</span>{'Frontend Developer '}<span>{'/>'}</span></h1>
+                </div>
+            </div>            
         </section>
     );
 };
